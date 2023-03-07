57d3f6e366ba15b3ebf28e203e9ca934ce74da71
carlossdiaas <dudu.hjcd@gmail.com>
2023-03-07 13:45:25 -0300

    estudando DOM

---
+++ Exercicios/ex008/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.6
+console.log (`Sua velocidade é de ${vel} km/h.`)
+if (vel > 60){ //condiçao simples
+    console.log (`Sua velocidade ultrapassou o limite da via! Você foi multado!!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
